d56f840c524f1a6adcd89c534870484fd8344042
nabecamel <wataru_418@icloud.com>
2024-04-08 15:18:00 +0900

    コンフリクトの修正

--- apps/user-api/app/models/order_details.py
+++
@@ -1,6 +0,0 @@
-from common.database.model_mixins.order_mixins import OrderDetailMixin
-from config.settings import Base
-
-
-class OrderDetail(Base, OrderDetailMixin):
-    __tablename__ = "order_details"
